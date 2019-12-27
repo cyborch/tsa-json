cdbe5621ec893b3d43169c86f7771a4f8c9e0c65
Anders Borch <anders@cyborch.com>
2019-12-27 15:34:32 +0100

    Cache loaded properties

--- src/main/kotlin/com/cyborch/tsajson/Config.kt
+++ src/main/kotlin/com/cyborch/tsajson/Config.kt
@@ -3,17 +3,21 @@ package com.cyborch.tsajson
 import java.io.FileInputStream
 import java.util.*
 
+private var props: Properties? = null
+
 /**
  * Server configuration.
  */
 fun config(): Properties {
-    val filename = "/opt/tsa/config/server.properties"
-    val props = Properties()
-    props.load(
+    if (props != null) return props!!
+    val pwd = System.getProperty("user.dir")
+    val filename = "${pwd}/config/server.properties"
+    props = Properties()
+    props?.load(
             FileInputStream(filename)
                 .reader(
                     charset("UTF-8")
                 )
         )
-    return props
+    return props!!
 }
